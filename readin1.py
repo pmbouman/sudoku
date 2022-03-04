b809b939a96107667ae552ec34a360f65f6b7bfc
Peter Bouman <pmbouman@gmail.com>
2022-03-03 18:49:59 -0600

    Added code to read Sudoku puzzle into dictionary data structure

--- readin1.py
+++ readin1.py
@@ -2,6 +2,11 @@
 
 import csv
 
+def init_clueset(cellchar):
+    if (cellchar == ""):
+        return full_clueset
+    else:
+        return {int(cellchar)}
 ### setup of variables
 
 readin_state = {}
@@ -10,19 +15,16 @@ full_clueset = set(range(1, 10)) ###
 
 f = open("puzzle1.csv")
 
-rownames = ["a", "b", "c", "d", "e", "f","g", "h", "i"]
-for letter in rownames:
-    print(letter)
+rownames = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+for rowname in rownames:
     puzzle_row = f.readline()
-    print(puzzle_row.rstrip().split(","))
+    row_entries = puzzle_row.rstrip().split(",")
 
-def init_clueset(cellchar):
-    if (cellchar == ""):
-        return full_clueset
-    else:
-        return {int(cellchar)}
+    for i in range(0, 9):
+        cellname = rowname + str(i+1)
+        entry = init_clueset(row_entries[i])
+        readin_state[cellname] = {"Marked": "Unmarked", "Allowed":entry}
 
-print(init_clueset("8"))
-print(init_clueset(""))
+print(readin_state)
 
 
